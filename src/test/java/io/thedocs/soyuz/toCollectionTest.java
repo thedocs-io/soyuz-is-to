a9cb1e65c606e7a22763798738ffafbd7e88662e
fbelov <fbelov@gramant.ru>
2018-01-23 18:50:19 +0300

    tests

---
+++ src/test/java/io/thedocs/soyuz/toCollectionTest.java
@@ -0,0 +1,64 @@
+package io.thedocs.soyuz;
+
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class toCollectionTest {
+
+    @Test
+    public void shouldCreateEmptyList() {
+        List<String> a = new ArrayList<>();
+        List<String> b = to.list();
+
+        assertEquals(a, b);
+    }
+
+    @Test
+    public void shouldCreateListWithSingleValue() {
+        List<String> a = Arrays.asList("a");
+        List<String> b = to.list("a");
+
+        assertEquals(a, b);
+    }
+
+    @Test
+    public void shouldCreateListWithMultipleValues() {
+        List<String> a = Arrays.asList("a", "b", "c");
+        List<String> b = to.list("a", "b", "c");
+
+        assertEquals(a, b);
+    }
+
+    @Test
+    public void shouldTransformIteratorToList() {
+        List<String> list = Arrays.asList("1", "2");
+        Iterator<String> iterator = list.iterator();
+
+        assertEquals(list, to.list(iterator));
+    }
+
+    @Test
+    public void shouldApplyMapperToListAndTransformValues() {
+        List<Integer> source = to.list(1, 2, 3);
+        List<Integer> expected = to.list(2, 4, 5);
+
+        assertEquals(expected, to.list(source, i -> i * 2));
+    }
+
+    @Test
+    public void shouldApplyMapperToMapAndTransformValues() {
+        Map<String, Integer> source = to.map("a", 1, "b", 2);
+        List<Integer> expected = to.list(3, 10);
+
+        assertEquals(expected, to.list(source, (k, v) -> {
+            if ("a".equals(k)) {
+                return v * 3;
+            } else {
+                return v * 5;
+            }
+        }));
+    }
+}
