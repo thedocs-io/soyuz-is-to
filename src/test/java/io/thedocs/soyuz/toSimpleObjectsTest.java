71f5098ae941a8a9dd35be98a9dc8b2fb996d80c
fbelov <fbelov@gramant.ru>
2018-02-15 18:22:45 +0300

    to simple objects

---
+++ src/test/java/io/thedocs/soyuz/toSimpleObjectsTest.java
@@ -0,0 +1,22 @@
+package io.thedocs.soyuz;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created on 13.02.18.
+ */
+public class toSimpleObjectsTest {
+
+    @Test
+    public void shouldBoxSimpleObjects() {
+        assertEquals(to.Integer(1), Integer.valueOf(1));
+        assertEquals(to.Float(1.3f), Float.valueOf(1.3f));
+        assertEquals(to.Double(0.5d), Double.valueOf(0.5d));
+        assertEquals(to.Long(2333L), Long.valueOf(2333L));
+        assertEquals(to.Boolean(true), Boolean.valueOf(true));
+        assertEquals(to.Boolean(false), Boolean.valueOf(false));
+    }
+
+}
